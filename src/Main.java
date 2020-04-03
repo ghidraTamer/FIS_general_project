628bc7d51bb6c150ec26838308b957476fb2b1ed
bogdan <bogdan>
2020-04-03 05:36:39 +0300

    committed pre-genered project files

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import sqlconnection.*;
+
+public class Main {
+    public static void main(String args[]) {
+        SQLiteJDBC sqlitejdbc = new SQLiteJDBC();
+        sqlitejdbc.connect();
+
+
+    }
+}
