0ae97bf399bf630960983ea966d5baace810d488
bogdan <bogdan>
2020-04-03 16:16:33 +0300

    created SQLite insert/select/create library

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 import sqlconnection.*;
 
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String args[]) {
         SQLiteJDBC sqlitejdbc = new SQLiteJDBC();
-        sqlitejdbc.connect();
+
+        //table name + values is hardcoded
+        sqlitejdbc.CreateRegistrationTable();
+        sqlitejdbc.insertRegistration("bogdan","boby","bogdanATyahoocom","bbogdanel","ancd");
+        sqlitejdbc.selectStatement("SELECT * FROM REGISTRATION");
 
 
     }
--- src/sqlconnection/SQLiteJDBC.java
+++ src/sqlconnection/SQLiteJDBC.java
@@ -1,14 +1,18 @@
 package sqlconnection;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class SQLiteJDBC {
+    private static Connection conn;
 
-    public static void connect() {
+
+    //connects to the tatabase
+    public static void connect(String nameDB) {
         try {
 
-            String url = "jdbc:sqlite:test.db";
+            String url = "jdbc:sqlite:"+nameDB;
             Class.forName("org.sqlite.JDBC");
-            Connection conn = DriverManager.getConnection(url);
+            conn = DriverManager.getConnection(url);
 
             System.out.println("CONNECTED");
         } catch (SQLException e) {
@@ -19,6 +23,75 @@ public class SQLiteJDBC {
         }
     }
 
+    public void CreateRegistrationTable() {
+        this.connect("test.db");
+
+        Statement stmt = null;
+        String sql = "CREATE TABLE IF NOT EXISTS REGISTRATION (\n"
+                + "    id integer PRIMARY KEY,\n"
+                + "    firstname text NOT NULL,\n"
+                + "    lastname text NOT NULL,\n"
+                + "    email text NOT NULL,\n"
+                + "    username text NOT NULL,\n"
+                + "    password text NOT NULL\n"
+                + ");";
+
+        try {
+            stmt = conn.createStatement();
+            stmt.execute(sql);
+            System.out.println("CREATED OPENED");
+
+            stmt.close();
+            conn.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+//makes a querry request to the database in search for a specified object in a specified table
+    //TO DO : make an enhanged version to select single entries
+    public void selectStatement(String selectData) {
+        this.connect("test.db");
+        Statement stmt = null;
+        String[] foundQuerry;
+
+        try {
+            stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(selectData);
+
+
+
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    //the insertion is hardcoded
+    public void insertRegistration(String firstname, String lastname, String email, String username, String password) {
+        this.connect("test.db");
+
+        Statement stmt = null;
+        String values = "(" + "'" +firstname + "'" + "," + "'"+lastname + "'" +"," + "'" + email + "'" + "," + "'" + username + "'" + "," + "'" + password+"'" + ")";
+        System.out.println(values);
+
+        try {
+            stmt = conn.createStatement();
+            stmt.executeUpdate("INSERT INTO REGISTRATION(firstname, lastname,email,username,password)"+ "VALUES"+values);
+            System.out.println("DATA INSERTED");
+
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+            }
 
 
 }
